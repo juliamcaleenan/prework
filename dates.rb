b7f057a7a7fc2b94b4d7c51638772f1cf7f9a484
Julia McAleenan <juliamcaleenan@gmail.com>
2014-10-16 10:57:17 +0100

    reorganised files

--- dates.rb
+++
@@ -1,5 +0,0 @@
-dates = [1980, 2000, 1967]
-
-puts dates[0]
-puts dates[1]
-puts dates[2]
--- movies_dates.rb
+++
@@ -1,7 +0,0 @@
-movies = {  grease: 1975,
-			love_actually: 2000,
-			jaws: 1980}
-
-puts movies[:grease]
-puts movies[:love_actually]
-puts movies[:jaws]
